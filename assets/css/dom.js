318c2bb5f12a4ae37e4944a20a204c7ed91323ae
Vagif34 <vagifrg@code.edu.az>
2022-10-13 17:48:22 +0400

    new task

---
+++ assets/css/dom.js
@@ -0,0 +1,18 @@
+const newUsername=document.querySelectorAll(".username");
+const newEmail=document.querySelectorAll(".email")
+const newPassword=document.querySelectorAll(".password")
+const buttton=document.querySelectorAll(".btn")
+document.getElementById("btn").addEventListener("click", printToScreen);
+
+
+btn.forEach(item => {
+    console.log(item);
+    item.onclick =()=>{
+        console.log();
+    }
+    
+});
+username.innertext
+function printToScreen(){
+        document.getinfo("").innertext=Info();
+}
